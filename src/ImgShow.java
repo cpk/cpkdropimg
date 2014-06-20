d330c01e1eef142abc2cea376ee05ebe341bc028
MrGru <duan.hut@gmail.com>
2014-06-20 22:18:45 +0700

    Test Image Show Class. First

---
+++ src/ImgShow.java
@@ -0,0 +1,27 @@
+
+import java.awt.Canvas;
+import java.awt.TrayIcon;
+import java.awt.image.BufferedImage;
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cuocs_000
+ */
+public class ImgShow extends Canvas {
+    public BufferedImage Bi;
+    public Point start;
+    public Point end;
+    public ShowJFrame newJFrame;
+    TrayIcon trayIcon;
+    
+    public ImgShow(){
+   
+    }
+    
+}
